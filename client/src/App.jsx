f7544c7078afafc1d42d4484cbb3b4f52729083d
Francisco Di Nucci <franciscoldinucci@gmail.com>
2023-06-18 20:20:51 -0300

    fix altura2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,8 +33,8 @@ function App() {
   return (
     <div className='h-screen bg-zinc-800 text-white flex flex-col items-center justify-center'>
       <h1 className='text-2xl font-bold my-2'>Proyecto</h1>
-      <form className='bg-zinc-900 flex flex-col justify-between h-2/3 w-1/2' onSubmit={handleSubmit}>
-          <ul className='p-2 max-h-96 overflow-y-auto'>
+      <form className='bg-zinc-900 flex flex-col justify-between min-h-2/3 h-fit w-1/2' onSubmit={handleSubmit}>
+          <ul className='p-2'>
             {messages.map((message, i) => (
               <li className={
                 `my-2 p-2 table rounded-md ${message.from === 'Me' ? 
